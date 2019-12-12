1b81570abc61eafa6c3fb644036d751dbde72a13
Jordan Fox <jordanmichaelfox@hotmail.com>
2019-12-11 19:13:04 -0800

    added unique function set

--- app.js
+++ app.js
@@ -17,19 +17,47 @@ function show(elem) {
 //useful 'button' site https://css-tricks.com/use-button-element/
 
 // 1. Create the button
-var button = document.createElement("button");
-button.innerHTML = "Do Something";
+var button = document.createElement('button');
+button.innerHTML = 'Do Something';
 
 // 2. Append somewhere
-var body = document.getElementsByTagName("body")[0];
+var body = document.getElementsByTagName('body')[0];
 body.appendChild(button);
 
 // 3. Add event handler
-button.addEventListener ("click", function() {
-  alert("did something");
+button.addEventListener ('click', function() {
+  alert('did something');
 });
 
 //useful 'form' site https://www.xul.fr/javascript/form-intro.php
 
 //unique functions list
 
+
+function getUnique() {
+  while (uniqueIndexes.length < 6) {
+    var random = randomIndex(picArray.length);
+    if (!uniqueIndexes.includes(random)) {
+      uniqueIndexes.push(random);
+    }
+  }
+}
+
+function removeThree() {
+  for (var i = 0; i < 3; i++) {
+    uniqueIndexes.shift();
+  }
+}
+
+function generateImages() {
+  getUnique();
+
+  for (var i = 0; i < pics.length; i++) {
+    pics[i].src = picArray[uniqueIndexes[i]].src;
+    pics[i].title = picArray[uniqueIndexes[i]].title;
+    pics[i].alt = picArray[uniqueIndexes[i]].alt;
+    picNames[i].textContent = picArray[uniqueIndexes[i]].title;
+
+    picArray[uniqueIndexes[i]].viewed++;
+  }
+}
